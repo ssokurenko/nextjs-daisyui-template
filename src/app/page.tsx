49b1db151806d21a0aa59385af54b9640a273986
Sergii Sokurenko <s.sokurenko@gmail.com>
2025-08-04 21:35:20 -0600

    Min update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <div className="hero min-h-96">
       <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Welcome!</h1>
+          <h1 className="text-5xl font-bold">Welcome to the Template project!</h1>
           <button className="btn btn-primary mt-4">Get Started</button>
         </div>
       </div>
